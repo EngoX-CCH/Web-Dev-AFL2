f17bcc5403ee16b44a5b3f07eb092dbba6e967e5
strawhatcrue1 <84302030+strawhatcrue1@users.noreply.github.com>
2022-11-10 12:57:11 +0700

    .

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,11 +1,12 @@
-@extends('Layout.MainLayout')
+@extends('Layout.layout')
 
 @section('container')
     <h1>{{$mainText}} </h1>
 
-    <img src="../{{ $perfumes['image']}}" style="width: 100px">
+    @foreach ($perfumes as $perfume)
+        <img src="../{{ $perfume['image']}}" style="width: 100px">
 
-    <h2>Name : {{ $perfumes['name'] }}</h2>
-    {{-- <h3>Contact : {{ $perfume['contact'] }}</h3> --}}
-
+        <h2>Name : {{ $perfume['name'] }}</h2>
+        <p>Description : {{ $perfume['description'] }}</p>
+    @endforeach
 @endsection
